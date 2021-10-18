30312abd0a1812c33dad6891489f04d7e4c9701e
DESKTOP-DUEFD85\user <mancapitalikarashi@gmail.com>
2021-10-18 09:52:37 +0900

    no message

---
+++ Views/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" type="text/css" href="/public/css/index.css">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"><!--虫眼鏡アイコンを表示するためのリンク-->
+  <title>このドキュメントのタイトル</title>
+</head>
+
+<body>
+<div id='contents'>
+    <div id='tab'>
+        <div class="flex items-center" id='iconline'>
+            <div id='icon'><img src="/public/img/icon.png"></div>
+            <div id='title'>コミュニティ</div>
+        </div>
+        <div id='home'>
+            <img src="/public/img/home.png">
+            <div>HOME</div>
+        </div>
+    </div>
+    <div id='main'>
+        <div id="main_title">home</div>
+        <div id='main_user'>ユーザー名</div>
+        <div id='textarea'><textarea name="text" rows="10" cols="60">テキストエリアです。</textarea></div>
+    </div>
+    <div id='sub'>
+        <form method="get" action="#" class="search_container">
+            <div class="center">
+                <input type="text" size="25" placeholder="キーワード検索">
+                <input type="submit" value="&#xf002">
+            </div>
+        </form>
+    </div>
+</div>
+<div></div>
+</body>
+</html>
